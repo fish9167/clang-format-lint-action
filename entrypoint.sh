3e820dc46759f4d6bc4cfe475f89a3d0a38b5a15
fish9167 <674821954@qq.com>
2021-05-12 14:56:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,3 @@
 cd "$GITHUB_WORKSPACE"
 
 /run-clang-format.py "$@"
-
-STATUS_ARGS=0
-PATHSPEC=$2
-
-function check() {
-  if ! git diff --quiet;
-  then
-    echo "0"
-  else
-    echo "1"
-  fi
-}
-
-echo ::set-output name=changed::$(check)
